8608262816e80e57a899c01c5cd70f8b019d1f8d
Mottyan123 <kazu.h9898@gmail.com>
2025-02-20 00:40:11 +0900

    データ通信のテストコード改良

--- examples/test_recv.ino
+++ examples/test_recv.ino
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 #include "CAN_Teensy4.0.h"
 
+#define __DEBUG__
+
 /*test_data*/
-uint8_t len;
+uint8_t len8;
+uint8_t len16;
+uint8_t lenF16;
 int8_t recv_data8[8];
 int16_t recv_data16[4];
 float recv_dataF16[4];
@@ -16,29 +20,32 @@ void setup() {
 }
 
 void loop() {
-    can_recv8(0x301, recv_data8, len);
+    can_recv8(0x301, recv_data8, len8); //8bit受信
+    can_recv16(0x302, recv_data16, len16); //16bit受信
+    can_recvF16(0x303, recv_dataF16, lenF16); //float16bit受信
+
+    #ifdef __DEBUG__
     Serial.print("recv_data8: ");
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len8; i++) {
         Serial.print(recv_data8[i]);
         Serial.print(" ");
     }
     Serial.println();
 
-    can_recv16(0x302, recv_data16, len);
     Serial.print("recv_data16: ");
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len16; i++) {
         Serial.print(recv_data16[i]);
         Serial.print(" ");
     }
     Serial.println();
 
-    can_recvF16(0x303, recv_dataF16, len);
     Serial.print("recv_dataU16: ");
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < lenF16; i++) {
         Serial.print(recv_dataF16[i]);
         Serial.print(" ");
     }
     Serial.println();
+    #endif
 
     delay(1000);
 }
--- examples/test_send.ino
+++ examples/test_send.ino
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include "CAN_Teensy4.0.h"
 
+#define __DEBUG__
+
 /*test_data*/
 int8_t send_data8[] = {0, 100, -100, 127, -128, 1, 2, 3};
 int16_t send_data16[] = {0, 10000, -10000, 32767};
 float send_dataF16[] = {0.0, 180.1234, 360.4567, 654.7890};
+uint8_t len8 = sizeof(send_data8)/sizeof(send_data8[0]);
+uint8_t len16 = sizeof(send_data16)/sizeof(send_data16[0]);
+uint8_t lenF16 = sizeof(send_dataF16)/sizeof(send_dataF16[0]);
 
 void setup() {
     Serial.begin(115200);
@@ -15,17 +20,40 @@ void setup() {
 }
 
 void loop() {
-    if (!can_send8(0x301, send_data8, 8)) {
+    if (!can_send8(0x301, send_data8, 8)) { //8bit送信
         Serial.println("can_send8 fail");
     }
 
-    if (!can_send16(0x302, send_data16, 4)) {
+    if (!can_send16(0x302, send_data16, 4)) { //16bit送信
         Serial.println("can_send16 fail");
     }
 
-    if (!can_sendF16(0x303, send_dataF16, 4)) {
+    if (!can_sendF16(0x303, send_dataF16, 4)) { //float16bit送信
         Serial.println("can_sendU16 fail");
     }
+
+    #ifdef __DEBUG__
+    Serial.print("send_data8: ");
+    for (int i = 0; i < len8; i++) {
+        Serial.print(send_data8[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
+
+    Serial.print("send_data16: ");
+    for (int i = 0; i < len16; i++) {
+        Serial.print(send_data16[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
+
+    Serial.print("send_dataU16: ");
+    for (int i = 0; i < lenF16; i++) {
+        Serial.print(send_dataF16[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
+    #endif
 
     delay(1000);
 }
