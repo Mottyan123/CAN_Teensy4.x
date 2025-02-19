3080ebeb282c4c34f3e1b6e52985c510ff9b4eee
Mottyan123 <kazu.h9898@gmail.com>
2025-02-20 00:13:32 +0900

    float型のCAN通信を実装

--- src/CAN_Teensy4.0.cpp
+++ src/CAN_Teensy4.0.cpp
@@ -1,5 +1,7 @@
 #include "CAN_Teensy4.0.h"
 
+const int SCALE = 100;
+
 /*onedeta_size: -128~127*/
 bool can_send8(uint16_t id, int8_t data[], uint8_t size) {
     if (size > 8) {
@@ -33,6 +35,29 @@ bool can_send16(uint16_t id, int16_t data[], uint8_t size) {
     return can2.write(msg);
 }
 
+/*onedeta_size: 0~654.xxx*/
+bool can_sendF16(uint16_t id, float data[], uint8_t size) {
+    uint16_t re_data[size];
+
+    if (size > 4) {
+        return false;
+    }
+
+    for (int i = 0; i < size; i++){
+        re_data[i] = (uint16_t)(data[i] * SCALE);
+    }
+
+    for (int j = 0; j < size; j++) {
+        msg.buf[j*2] = (uint8_t)(re_data[j] & 0xFF);
+        msg.buf[j*2+1] = (uint8_t)((re_data[j] >> 8) & 0xFF);
+    }
+
+    msg.id = id;
+    msg.len = size*2;
+
+    return can2.write(msg);
+}
+
 /*8bit_data 受信*/
 void can_recv8(uint16_t id, int8_t data[], uint8_t &len) {
     if (can2.read(msg)) {
@@ -56,6 +81,24 @@ void can_recv16(uint16_t id, int16_t data[], uint8_t &len) {
 
             for (int i = 0; i < size/2; i++) {
                 data[i] = (int16_t)((msg.buf[i*2+1] << 8) | msg.buf[i*2]);
+            }
+        }
+    }
+}
+
+void can_recvF16(uint16_t id, float data[], uint8_t &len) {
+    if (can2.read(msg)) {
+        if (msg.id == id) {
+            uint8_t size = msg.len;
+            len = size/2;
+            uint16_t re_data[len];
+
+            for (int i = 0; i < size/2; i++) {            
+                re_data[i] = (uint16_t)((msg.buf[i*2+1] << 8) | msg.buf[i*2]);
+            }
+
+            for (int j = 0; j < size/2; j++) {
+                data[j] = (float)re_data[j] / SCALE;
             }
         }
     }
--- src/CAN_Teensy4.0.h
+++ src/CAN_Teensy4.0.h
@@ -8,8 +8,10 @@
 
 bool can_send8(uint16_t id /*CAN ID*/, int8_t data[] /*-128~127*/, uint8_t size);
 bool can_send16(uint16_t id /*CAN ID*/, int16_t data[] /*-32768~32767*/, uint8_t size);
+bool can_sendF16(uint16_t id /*CAN ID*/, float data[] /*0~654.xxx*/, uint8_t size);
 
 void can_recv8(uint16_t id /*CAN ID*/, int8_t data[] /*-128~127*/, uint8_t &len);
 void can_recv16(uint16_t id /*CAN ID*/, int16_t data[] /*-32768~32767*/, uint8_t &len);
+void can_recvF16(uint16_t id /*CAN ID*/, float data[] /*0~654.xxx*/, uint8_t &len);
 
 #endif
